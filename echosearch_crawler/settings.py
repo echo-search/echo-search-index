32ddf86822b3c51d00c7f156cb4a6906ad64daec
echo-search <hassaanulhoda@gmail.com>
2026-03-29 19:50:35 +0100

    Update settings.py

--- echosearch_crawler/settings.py
+++ echosearch_crawler/settings.py
@@ -1,16 +1,18 @@
+import os
+
 BOT_NAME = "echosearch_crawler"
 
 SPIDER_MODULES = ["echosearch_crawler.spiders"]
 NEWSPIDER_MODULE = "echosearch_crawler.spiders"
 
-# -----------------------------
+# ------------------------------------------------
 # RESPECT ROBOTS.TXT
-# -----------------------------
+# ------------------------------------------------
 ROBOTSTXT_OBEY = True
 
-# -----------------------------
+# ------------------------------------------------
 # SPEED + SAFETY
-# -----------------------------
+# ------------------------------------------------
 DOWNLOAD_DELAY = 0.5
 CONCURRENT_REQUESTS_PER_DOMAIN = 2
 
@@ -19,27 +21,37 @@ AUTOTHROTTLE_START_DELAY = 0.5
 AUTOTHROTTLE_TARGET_CONCURRENCY = 3.0
 AUTOTHROTTLE_MAX_DELAY = 10
 
-# -----------------------------
+# ------------------------------------------------
 # PREVENT 6-HR GITHUB CANCELLATION
-# -----------------------------
-CLOSESPIDER_PAGECOUNT = 100    # you can change this
-CLOSESPIDER_TIMEOUT = 20701         # ~5.5 hours
+# ------------------------------------------------
+CLOSESPIDER_PAGECOUNT = 100
+CLOSESPIDER_TIMEOUT = 20701
 
-# -----------------------------
-# PIPELINES (ES + DEDUP)
-# -----------------------------
-ITEM_PIPELINES = {
-    'echosearch_crawler.pipelines.DedupPipeline': 200,
-}
-
-# -----------------------------
-# DISABLE FEED OUTPUT (IMPORTANT)
-# -----------------------------
-FEEDS = {
-    'crawl_output.json': {
-        'format': 'json',
-        'encoding': 'utf8',
-        'indent': 0,
-        'overwrite': True,
+# ------------------------------------------------
+# PIPELINES (DIFFERENT for Codespaces vs Actions)
+# ------------------------------------------------
+if os.getenv("GITHUB_ACTIONS"):
+    # GitHub Actions: NO Elasticsearch, only dedup
+    ITEM_PIPELINES = {
+        'echosearch_crawler.pipelines.DedupPipeline': 200,
     }
-}
+
+    # GitHub Actions: write crawl_output.json
+    FEEDS = {
+        'crawl_output.json': {
+            'format': 'json',
+            'encoding': 'utf8',
+            'indent': 0,
+            'overwrite': True,
+        }
+    }
+
+else:
+    # Codespaces: USE Elasticsearch pipeline
+    ITEM_PIPELINES = {
+        'echosearch_crawler.pipelines.DedupPipeline': 200,
+        'echosearch_crawler.pipelines.ElasticsearchPipeline': 300,
+    }
+
+    # Codespaces: disable JSON feed
+    FEEDS = {}
